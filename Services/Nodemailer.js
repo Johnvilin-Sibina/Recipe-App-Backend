21a0e548d1767d24959d7918ccff1b0e86c523f7
Johnvilin-Sibina <johnvilinsibina@gmail.com>
2025-03-24 14:43:12 +0530

    Added frontend deploed url.

--- Services/Nodemailer.js
+++ Services/Nodemailer.js
@@ -21,8 +21,8 @@ export const sendLink = async (email, token, userId) => {
       from: process.env.PASSMAIL,
       to: email,
       subject: "Reset Password",
-      text: `http://localhost:5173/resetpassword/${userId}/${token}`,
-      html: `<p>A request is made to reset your password. If it is made by you click the following link to proceed: <a href=http://localhost:5173/resetpassword/${userId}/${token}">Reset Password</a>. If it is not done by you, ignore the mail.</p>`,
+      text: `https://tasty-trove-recipes.netlify.app/resetpassword/${userId}/${token}`,
+      html: `<p>A request is made to reset your password. If it is made by you click the following link to proceed: <a href=https://tasty-trove-recipes.netlify.app/resetpassword/${userId}/${token}">Reset Password</a>. If it is not done by you, ignore the mail.</p>`,
     });
     console.log("Mail Sent Successfully:", info.response);
   } catch (error) {
